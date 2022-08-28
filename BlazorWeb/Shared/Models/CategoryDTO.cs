a4863f38aec08e9732818307563e6c9ef98bd4a8
LightningBits <LIghtnigbits@lightningbits.com>
2022-08-27 21:55:10 -0400

    add category CRUD

--- BlazorWeb/Shared/Models/CategoryDTO.cs
+++ BlazorWeb/Shared/Models/CategoryDTO.cs
@@ -1,12 +1,16 @@
 ﻿// LightningBits
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorWeb.Shared
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Please enter namey")]
         public string? Name { get; set; }
+
     }
 }
 
