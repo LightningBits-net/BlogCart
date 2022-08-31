4bb4cddc9f1499c7c95a6459b9460f8d91084f28
LightningBits <LIghtnigbits@lightningbits.com>
2022-08-31 13:26:49 -0400

    remove server

--- BlazorWeb/Client/Program.cs
+++ BlazorWeb/Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using BlazorWeb.Client;
 using MudBlazor.Services;
 
@@ -9,7 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ICategoryRespository, CategoryRespository>();
 
 await builder.Build().RunAsync();
 
