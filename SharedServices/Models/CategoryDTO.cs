69efc0b5901dec6d3c42eb4e9dd2af09caedc82a
LightningBits <LIghtnigbits@lightningbits.com>
2022-09-02 11:30:04 -0400

    update To server UI

--- SharedServices/Models/CategoryDTO.cs
+++ SharedServices/Models/CategoryDTO.cs
@@ -9,7 +9,7 @@ namespace SharedServices
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Please enter name")]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
     }
 }
