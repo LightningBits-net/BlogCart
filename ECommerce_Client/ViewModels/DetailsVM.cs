b92db340f8370b349d22fd4d68461136d56b6014
LightningBits <LIghtnigbits@lightningbits.com>
2022-11-07 14:41:55 -0500

    Update Readme  and some UI

---
+++ ECommerce_Client/ViewModels/DetailsVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using SharedServices.Models;
+
+namespace ECommerce_Client.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            ProductPrice = new();
+            Count = 1;
+        }
+            
+        [Range(1, int.MaxValue, ErrorMessage ="Please enter a vlue greater than 0")]
+       public int Count { get; set; }
+
+        [Required]
+       public int SelectedProductPriceId { get; set; }
+
+        public ProductPriceDTO ProductPrice { get; set; }
+    }
+}
+
