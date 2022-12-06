41df04cc1a8a622577b47b19650a5f26a1b63db5
LightningBits <LIghtnigbits@lightningbits.com>
2022-12-05 22:07:43 -0500

    Add Identity Roles

--- ECommerce_Server/Program.cs
+++ ECommerce_Server/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders().AddDefaultUI().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductPriceRepository, ProductPriceRepository>();
--- SharedServices/Commons/SD.cs
+++ SharedServices/Commons/SD.cs
@@ -10,6 +10,9 @@ namespace SharedServices.Commons
         public const string Status_Shipped = "Shipped";
         public const string Status_Refunded = "Refunded";
         public const string Status_Cancelled = "Cancelled";
+
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
     }
 }
 
