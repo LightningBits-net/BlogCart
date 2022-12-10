2cd9dd882e9916801132141c42a1f1816abd5028
LightningBits <LIghtnigbits@lightningbits.com>
2022-12-09 23:32:02 -0500

    Update Email Expression

--- SharedServices/Models/SignUpRequestDTO.cs
+++ SharedServices/Models/SignUpRequestDTO.cs
@@ -3,13 +3,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SharedServices.Models
 {
-	public class SignUpRequestDTO
-	{
+    public class SignUpRequestDTO
+
+    {
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
-        [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email address")]
+        //[RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email address")]
+        [EmailAddress]
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
@@ -24,5 +26,6 @@ namespace SharedServices.Models
         [Compare("Password", ErrorMessage = "Password and confirm password is not matched")]
         public string ConfirmPassword { get; set; }
     }
+
 }
 
