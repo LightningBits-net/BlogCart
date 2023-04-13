6e86314fcfc50fd451fa1031432c755394fdab56
LightningBits <LIghtnigbits@lightningbits.com>
2023-04-12 21:34:08 -0400

    Update Navigation between domains

--- src/ECommerce_Server/Constants.cs
+++ src/ECommerce_Server/Constants.cs
@@ -4,14 +4,14 @@ public static class Constants
 {
     //Production
 
-    //public static string MyStoreHost { get; set; } = "https://Mystore.lightningbits.net";
-    //public static string RootHost { get; set; } = "https://lightningbits.net";
-    //public static string MyBlogHost { get; set; } = "https://myblog.lightningbits.net";
+    public static string MyStoreHost { get; set; } = "https://Mystore.lightningbits.net";
+    public static string RootHost { get; set; } = "https://lightningbits.net";
+    public static string MyBlogHost { get; set; } = "https://myblog.lightningbits.net";
 
     //developoment
 
-    public static string MyStoreHost { get; set; } = "https://localhost:5000";
-    public static string RootHost { get; set; } = "https://localhost:5199";
-    public static string MyBlogHost { get; set; } = "https://localhost:5199";
+    //public static string MyStoreHost { get; set; } = "https://localhost:5000";
+    //public static string RootHost { get; set; } = "https://localhost:5199";
+    //public static string MyBlogHost { get; set; } = "https://localhost:5199";
 }
 
