089fc0c06bc8f872ceadbcc5d1b25fd0c075c96a
LightningBits <LIghtnigbits@lightningbits.com>
2023-04-15 11:01:23 -0400

    Create Constants to handle URL nav

--- BlogCart/Constants.cs
+++ BlogCart/Constants.cs
@@ -2,16 +2,13 @@
 
 public static class Constants
 {
-    //Production
-
     public static string MyStoreHost { get; set; } = "https://Mystore.lightningbits.net";
     public static string RootHost { get; set; } = "https://lightningbits.net";
     public static string MyBlogHost { get; set; } = "https://myblog.lightningbits.net";
 
-    //developoment
-
-    //public static string MyStoreHost { get; set; } = "https://localhost:7099";
+    //public static string MystoreHost { get; set; } = "https://localhost:7099";
     //public static string RootHost { get; set; } = "https://localhost:5099";
-    //public static string MyBlogHost { get; set; } = "https://localhost:5099";
+    //public static string BlogHost { get; set; } = "https://localhost:5099";
+
 }
 
