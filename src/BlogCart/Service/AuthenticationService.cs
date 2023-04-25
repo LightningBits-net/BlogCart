d13120d2b29017dfead5a52e9c5e2cff3223d71d
Lightningbits <rafaelmorel@lightningbits.com>
2023-04-25 11:12:39 -0400

    Migration from webasembly to Server continued

---
+++ src/BlogCart/Service/AuthenticationService.cs
@@ -0,0 +1,78 @@
+﻿//using System;
+//using Blazored.LocalStorage;
+//using Microsoft.AspNetCore.Components.Authorization;
+//using Newtonsoft.Json;
+//using SharedServices.Commons;
+//using SharedServices.Models;
+//using System.Net.Http.Headers;
+//using System.Text;
+//using ECommerce_Client.Service.IService;
+
+
+//namespace ECommerce_Client.Service
+//{
+//    public class AuthenticationService : IAuthenticationService
+//    {
+//        private readonly HttpClient _client;
+//        private readonly ILocalStorageService _localStorage;
+//        private readonly AuthenticationStateProvider _authStateProvider;
+
+//        public AuthenticationService(HttpClient client, ILocalStorageService localStorage, AuthenticationStateProvider authStateProvider)
+//        {
+//            _client = client;
+//            _authStateProvider = authStateProvider;
+//            _localStorage = localStorage;
+//        }
+
+//        public async Task<SignInResponseDTO> Login(SignInRequestDTO signInRequest)
+//        {
+//            var content = JsonConvert.SerializeObject(signInRequest);
+//            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+//            var response = await _client.PostAsync("api/account/signin", bodyContent);
+//            var contentTemp = await response.Content.ReadAsStringAsync();
+//            var result = JsonConvert.DeserializeObject<SignInResponseDTO>(contentTemp);
+
+//            if (response.IsSuccessStatusCode)
+//            {
+//                await _localStorage.SetItemAsync(SD.Local_Token, result.Token);
+//                await _localStorage.SetItemAsync(SD.Local_UserDetails, result.UserDTO);
+//                ((AuthStateProvider)_authStateProvider).NotifyUserLoggedIn(result.Token);
+//                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Token);
+//                return new SignInResponseDTO() { IsAuthSuccessful = true };
+//            }
+//            else
+//            {
+//                return result;
+//            }
+//        }
+
+//        public async Task Logout()
+//        {
+//            await _localStorage.RemoveItemAsync(SD.Local_Token);
+//            await _localStorage.RemoveItemAsync(SD.Local_UserDetails);
+
+//            ((AuthStateProvider)_authStateProvider).NotifyUserLogout();
+
+//            _client.DefaultRequestHeaders.Authorization = null;
+//        }
+
+//        public async Task<SignUpResponseDTO> RegisterUser(SignUpRequestDTO signUpRequest)
+//        {
+//            var content = JsonConvert.SerializeObject(signUpRequest);
+//            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+//            var response = await _client.PostAsync("api/account/signup", bodyContent);
+//            var contentTemp = await response.Content.ReadAsStringAsync();
+//            var result = JsonConvert.DeserializeObject<SignUpResponseDTO>(contentTemp);
+
+//            if (response.IsSuccessStatusCode)
+//            {
+//                return new SignUpResponseDTO { IsRegisterationSuccessful = true };
+//            }
+//            else
+//            {
+//                return new SignUpResponseDTO { IsRegisterationSuccessful = false, Errors = result.Errors };
+//            }
+//        }
+//    }
+//}
+
