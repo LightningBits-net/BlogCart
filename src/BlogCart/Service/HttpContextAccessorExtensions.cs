9261c4a462d5d5d5226a81a7f83ee3aba25eb4fe
Lightningbits <rafaelmorel@lightningbits.com>
2023-04-30 22:56:28 -0400

    Unused Code

--- src/BlogCart/Service/HttpContextAccessorExtensions.cs
+++ src/BlogCart/Service/HttpContextAccessorExtensions.cs
@@ -1,13 +1,13 @@
-﻿using System;
-namespace BlogCart.Service
-{
-    public static class HttpContextAccessorExtensions
-    {
-        public static IServiceCollection AddCustomHttpContextAccessor(this IServiceCollection services)
-        {
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            return services;
-        }
-    }
-}
+﻿//using System;
+//namespace BlogCart.Service
+//{
+//    public static class HttpContextAccessorExtensions
+//    {
+//        public static IServiceCollection AddCustomHttpContextAccessor(this IServiceCollection services)
+//        {
+//            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//            return services;
+//        }
+//    }
+//}
 
