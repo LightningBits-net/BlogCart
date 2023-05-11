a4207d9d01214bc1d0ebdd8a87eaaa61a4489273
Lightningbits <rafaelmorel@lightningbits.com>
2023-05-11 10:58:08 -0400

    Add client identity role

---
+++ src/SharedServices/Migrations/20230511002157_ClientIdentityRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharedServices.Migrations
+{
+    public partial class ClientIdentityRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES ('CLIf8baf-26b5-4bdc-982d-6f30017f5c4c', 'Client', 'CLIENT', '')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [AspNetRoles] WHERE [Id] = 'CLIf8baf-26b5-4bdc-982d-6f30017f5c4c'");
+        }
+    }
+}
