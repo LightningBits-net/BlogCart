acbc1f2f515107e4d2d200de121334b4e15fb6c8
Lightningbits <rafaelmorel@lightningbits.com>
2023-05-15 08:56:48 -0400

    Create 20230514022007_AddfloatToBlogView.cs

---
+++ src/SharedServices/Migrations/20230514022007_AddfloatToBlogView.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharedServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddfloatToBlogView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Views",
+                table: "Blogs",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Views",
+                table: "Blogs",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
