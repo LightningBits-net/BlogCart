799fb7afb1bbee4a4ea7f457ddbe4824951e040c
Lightningbits <rafaelmorel@lightningbits.com>
2023-05-20 08:46:13 -0400

    Update Client in db revert some prev changes
    Commented out most code here will return at a later date

--- src/SharedServices/Data/Client.cs
+++ src/SharedServices/Data/Client.cs
@@ -8,7 +8,7 @@ namespace SharedServices.Data
     public class Client
     {
         [Key]
-        public int? ClientId { get; set; }
+        public int ClientId { get; set; }
 
         public string Name { get; set; }
 
@@ -22,20 +22,19 @@ namespace SharedServices.Data
 
         public string Email { get; set; }
 
-        public float? Counter { get; set; }
+        public float Counter { get; set; }
 
         public string ImageUrl { get; set; }
 
-        public bool? IsActive { get; set; }
+        public bool IsActive { get; set; }
 
         public string BillingCycle { get; set; }
 
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal? BillingAmount { get; set; }
+        public decimal BillingAmount { get; set; }
 
-        public DateTime? BillingStartDate { get; set; }
+        public DateTime BillingStartDate { get; set; }
 
-        public DateTime? BillingEndDate { get; set; }
+        public DateTime BillingEndDate { get; set; }
     }
 }
 
---
+++ src/SharedServices/Migrations/20230520121328_RevertUpdateClientToAllowNulls.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharedServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class RevertUpdateClientToAllowNulls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Clients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Counter",
+                table: "Clients",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BillingStartDate",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BillingEndDate",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BillingAmount",
+                table: "Clients",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Clients",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Counter",
+                table: "Clients",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BillingStartDate",
+                table: "Clients",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BillingEndDate",
+                table: "Clients",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BillingAmount",
+                table: "Clients",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
